52f6506b065b1b4d464ed92f509e8cf19e47e72f
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2023-01-19 22:02:55 +0545

    redefining homescreen

--- development/main.py
+++ development/main.py
@@ -1,10 +1,6 @@
 """Creating the base of the app, development in the dev branch"""
 from kivy.app import App  
-from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
-
-class MainScreen(BoxLayout):
-    pass
 
 class LabsApp(App):
     pass
