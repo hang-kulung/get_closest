95c77e1924eb49609f4143f41797f3fb1ce1f35e
Itsuren Kanakueta <itsurenkanakueta@gmail.com>
2023-01-20 00:53:50 +0545

    added MenuScreen

--- development/main.py
+++ development/main.py
@@ -1,6 +1,14 @@
 """Creating the base of the app, development in the dev branch"""
 from kivy.app import App  
+from kivy.uix.stacklayout import StackLayout
+from kivy.uix.label import Label
 
+class NumberDisplay(StackLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        for i in range(5):
+            l=Label(text="0",size_hint=(None,None),size=("20dp","20dp"))
+            self.add_widget(l)
 class LabsApp(App):
     pass
 
